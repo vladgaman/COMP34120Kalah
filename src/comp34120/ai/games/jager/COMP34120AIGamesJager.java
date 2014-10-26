2f6e225337b3a66f5df8c1f650e69cbec97cb606
avser_000 <avser_000@andrei>
2014-10-26 13:58:03 +0000

    start of project

---
+++ src/comp34120/ai/games/jager/COMP34120AIGamesJager.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp34120.ai.games.jager;
+
+/**
+ *
+ * @author avser_000
+ */
+public class COMP34120AIGamesJager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
